4f1fe2893dc7c7bf0f1c69d5e33853ad86a06727
ronjamyhr <ronja_myhr@hotmail.com>
2019-06-11 09:25:08 +0200

    Create error page and implement content and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { DetailsComponent } from './details/details.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { AdminComponent } from './admin/admin.component';
 import { FinishComponent } from './finish/finish.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   { path: '', component: ShowproductsComponent },
   { path: 'details/:id', component: DetailsComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'finish', component: FinishComponent }
+  { path: 'finish', component: FinishComponent },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
